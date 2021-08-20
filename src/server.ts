7c649c288ef1c3e8a81f3f37fd0bedbad32660cb
Luis Felipe de Paula Costa <lfbmrj15@gmail.com>
2021-08-20 13:01:37 -0300

    Configurado Debbuger para o vscode

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,8 @@ const app = express();
 
 app.use(express.json());
 
-app.get('',(request ,response)=>{
-  return  response.json({message:"Hello World"})
-})
-
-app.listen(3333, ()=>{
-  console.log("Server Started on Port 3333 😁")
+app.get('', (request, response) => {
+  return response.json({ message: 'Hello World' });
 });
+
+app.listen(3333);
